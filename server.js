f24966879d567fcf347220398910a80a55c01aa7
erick2014 <erickarmandogarcia@hotmail.com>
2019-05-24 18:56:15 -0500

    add missing dependencies and validations in server

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
+const corser = require('corser');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-const server = (dbInstance) => {
-  console.log('connection has been established successfully');
+// auth stuff
+const auth = require('./Controllers/authController.js')
+
+const server = (dbInstance = null) => {
   //remove x-powered-by from response( this is to improve performance a little bit in the server)
   app.disable('etag').disable('x-powered-by');
+
   //initialize body parser to receive parameters within a request
   app.use(bodyParser.urlencoded({ extended: true }));
+
   // parse application/json
   app.use(bodyParser.json())
+
   //enable cors
   app.use(corser.create());
 
-  //pass the sequelize instance through the request object
-  app.use((req, _, next) => {
-    req['db'] = dbInstance;
-    next();
-  })
-
   //token validator middleware
   app.use(auth.tokenValidationMiddleware)
-  //pass the sequelize instance through the request object
-  app.use((req, _, next) => {
-    req['db'] = dbInstance;
-    next();
-  })
 
-  // USERS ROUTE
+  //pass the sequelize instance through the request object
+  if (dbInstance) {
+    app.use((req, _, next) => {
+      req['db'] = dbInstance;
+      next();
+    })
+  }
+
+  // Routes
   require('./routes/user')(app, dbInstance);
 
   app.listen(4000);
